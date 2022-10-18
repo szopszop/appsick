18fc65cc7d2430d3631946166810700465afc2b4
Ochota Dariusz <ochota.dariusz@gmail.com>
2022-10-18 15:01:38 +0200

    Filled Visit model fields

--- src/main/java/com/codecool/appsick/model/Visit.java
+++ src/main/java/com/codecool/appsick/model/Visit.java
@@ -1,4 +1,34 @@
 package com.codecool.appsick.model;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class Visit {
+
+    private UUID visitId;
+    private UUID patientId;
+    private UUID doctorId;
+    private UUID clinicId;
+    private LocalDate date;
+    private boolean online;
+    private String reason;
+    private VisitStatus status;
+
+    public enum VisitStatus {
+        PENDING,
+        MISSED,
+        MOVED,
+        COMPLETED;
+    }
+
+    public Visit(UUID patientId, UUID doctorId, UUID clinicId, LocalDate date, boolean online, String reason, VisitStatus status) {
+        this.visitId = UUID.randomUUID();
+        this.patientId = patientId;
+        this.doctorId = doctorId;
+        this.clinicId = clinicId;
+        this.date = date;
+        this.online = online;
+        this.reason = reason;
+        this.status = status;
+    }
 }
