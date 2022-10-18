c3299432bc641a2150afc0e9179807d49fbd6ac2
Ochota Dariusz <ochota.dariusz@gmail.com>
2022-10-18 14:44:17 +0200

    Filled model fields for MedicalData object

--- src/main/java/com/codecool/appsick/model/MedicalData.java
+++ src/main/java/com/codecool/appsick/model/MedicalData.java
@@ -1,5 +1,16 @@
 package com.codecool.appsick.model;
 
+import java.util.UUID;
+
 public class MedicalData {
+
+    private UUID medicalDataId;
+    private UUID patientId;
+    private int weight;
+    private int height;
+    private String medicalConditions;
+    private String allergies;
+    private String addictions;
+    private String medicaments;
 
 }
