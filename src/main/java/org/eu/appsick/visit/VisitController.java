47f245ced0c0786edc84747d755287c01f29221c
Sebastian Dudkowski <sebastian.dudkowski@gmail.com>
2022-11-02 17:20:11 +0100

    define endpoints

--- src/main/java/org/eu/appsick/visit/VisitController.java
+++ src/main/java/org/eu/appsick/visit/VisitController.java
@@ -1,4 +1,29 @@
 package org.eu.appsick.visit;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/visit")
 public class VisitController {
+
+    // TODO: dependency injection
+    private final VisitDao visitDao = new VisitDaoOrm();
+    @GetMapping(value = "/{visit_id}", produces = "application/json")
+    public String getVisit(@PathVariable String visit_id){
+        return null;
+    }
+    @PostMapping("/patient/{patient_id}")
+    public String postVisit(@PathVariable String patient_id){
+        return null;
+    }
+
+    @PutMapping("/{visit_id}")
+    public String putVisit(@PathVariable String visit_id){
+        return null;
+    }
+
+    @DeleteMapping("/{visit_id}")
+    public String deleteVisit(@PathVariable String visit_id){
+        return null;
+    }
 }
--- src/main/java/org/eu/appsick/visit/VisitService.java
+++ src/main/java/org/eu/appsick/visit/VisitService.java
@@ -1,4 +1,19 @@
 package org.eu.appsick.visit;
 
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
 public class VisitService {
+
+    // TODO: dependency injection
+    private final VisitDao visitDao = new VisitDaoOrm();
+
+    public Visit getById(String id){
+        UUID uuid = UUID.fromString(id);
+        Visit visit = visitDao.getVisit(uuid);
+        return visit;
+    }
+
 }
