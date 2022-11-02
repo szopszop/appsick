5c9a4236d4fbd5e136f78e5865ea08406a0a266a
Sebastian Dudkowski <sebastian.dudkowski@gmail.com>
2022-11-02 11:58:49 +0100

    add visit interface and memory implementation

--- src/main/java/org/eu/appsick/visit/VisitDao.java
+++ src/main/java/org/eu/appsick/visit/VisitDao.java
@@ -1,4 +1,18 @@
 package org.eu.appsick.visit;
 
+import org.eu.appsick.user.doctor.Doctor;
+import org.eu.appsick.user.patient.Patient;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface VisitDao {
+
+    Visit getVisit(UUID visitId);
+    List<Visit> getVisitList(Patient patient);
+    List<Visit> getVisitList(Doctor doctor);
+    void addVisit(Visit visit);
+    void deleteVisit(Visit visit);
+
+
 }
--- src/main/java/org/eu/appsick/visit/VisitDaoOrm.java
+++ src/main/java/org/eu/appsick/visit/VisitDaoOrm.java
@@ -1,4 +1,61 @@
 package org.eu.appsick.visit;
 
+import org.eu.appsick.user.doctor.Doctor;
+import org.eu.appsick.user.patient.Patient;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class VisitDaoOrm implements VisitDao {
+
+    private final List<Visit> visitList;
+
+    public VisitDaoOrm(){
+        this.visitList = new ArrayList<>();
+    }
+
+
+    @Override
+    public Visit getVisit(UUID visitId) {
+        for (Visit visit : visitList){
+            if (visit.getVisitId().equals(visitId)){
+                return visit;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<Visit> getVisitList(Patient patient) {
+        List<Visit> visits = new ArrayList<>();
+        for (Visit visit : visitList){
+            if (visit.getPatientId().equals(patient.getUserId())){
+                visits.add(visit);
+            }
+        }
+        return visits;
+    }
+
+    @Override
+    public List<Visit> getVisitList(Doctor doctor) {
+        List<Visit> visits = new ArrayList<>();
+        for (Visit visit : visitList){
+            if (visit.getPatientId().equals(doctor.getUserId())){
+                visits.add(visit);
+            }
+        }
+        return visits;
+    }
+
+    @Override
+    public void addVisit(Visit visit) {
+        visitList.add(visit);
+    }
+
+    @Override
+    public void deleteVisit(Visit visit) {
+        visitList.remove(visit);
+    }
+
 }
