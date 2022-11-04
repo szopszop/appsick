eee33e7280b15be343cafff3dc68f25260f550db
Sebastian Dudkowski <sebastian.dudkowski@gmail.com>
2022-11-04 13:09:55 +0100

    add VisitService interface

--- src/main/java/org/eu/appsick/visit/VisitController.java
+++ src/main/java/org/eu/appsick/visit/VisitController.java
@@ -1,6 +1,5 @@
 package org.eu.appsick.visit;
 
-import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,45 +11,44 @@ import java.util.UUID;
 @RequestMapping("/api/visit")
 public class VisitController {
 
-    private final VisitService visitService;
-    private final Gson gson = new Gson();
+    private final MyVisitService myVisitService;
 
     @Autowired
-    public VisitController(VisitService visitService) {
-        this.visitService = visitService;
+    public VisitController(MyVisitService myVisitService) {
+        this.myVisitService = myVisitService;
     }
 
     @GetMapping(value = "/{visit_id}", produces = "application/json")
     public String getVisit(@PathVariable UUID visit_id) {
-        return visitService.getById(visit_id).toString();
+        return myVisitService.getById(visit_id).toString();
     }
 
     @GetMapping(value = "/doctor/{doctor_id}")
     public String getDoctorVisits(@PathVariable UUID doctor_id) {
-        return visitService.getDoctorVisits(doctor_id).toString();
+        return myVisitService.getDoctorVisits(doctor_id).toString();
     }
 
     @GetMapping(value = "/patient/{patient_id}")
     public String getVisits(@PathVariable UUID patient_id) {
-        return visitService.getPatientVisits(patient_id).toString();
+        return myVisitService.getPatientVisits(patient_id).toString();
     }
 
     @PostMapping()
     public ResponseEntity<String> postVisit(@RequestBody Visit newVisit) {
-        visitService.addVisit(newVisit);
+        myVisitService.addVisit(newVisit);
         return new ResponseEntity<>("Visit successfully added to database with visitId: " + newVisit.getVisitId(), HttpStatus.OK);
     }
 
     @PatchMapping(value = "/{visit_id}")
     public ResponseEntity<String> patchVisit(@PathVariable String visit_id, @RequestBody Visit editedVisit) {
-        return (visitService.editVisit(UUID.fromString(visit_id), editedVisit)) ?
+        return (myVisitService.editVisit(UUID.fromString(visit_id), editedVisit)) ?
                 new ResponseEntity<>("Visit successfully updated", HttpStatus.OK) :
                 new ResponseEntity<>("Visit doesn't exist", HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping(value = "/{visit_id}")
     public ResponseEntity<String> deleteVisit(@PathVariable String visit_id) {
-        visitService.deleteVisit(UUID.fromString(visit_id));
+        myVisitService.deleteVisit(UUID.fromString(visit_id));
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/org/eu/appsick/visit/VisitService.java
+++ src/main/java/org/eu/appsick/visit/VisitService.java
@@ -1,61 +1,15 @@
 package org.eu.appsick.visit;
 
-import org.eu.appsick.user.doctor.Doctor;
-import org.eu.appsick.user.doctor.DoctorDao;
-import org.eu.appsick.user.patient.Patient;
-import org.eu.appsick.user.patient.PatientDao;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
-@Service
-public class VisitService {
-    private final VisitDao visitDao;
-    private final PatientDao patientDao;
-    private final DoctorDao doctorDao;
+public interface VisitService {
 
-    @Autowired
-    public VisitService(VisitDao visitDao, PatientDao patientDao, DoctorDao doctorDao) {
-        this.visitDao = visitDao;
-        this.patientDao = patientDao;
-        this.doctorDao = doctorDao;
-    }
-
-    public Visit getById(UUID id) {
-        return visitDao.getVisit(id);
-    }
-
-    public List<Visit> getPatientVisits(UUID patientId) {
-        Patient patient = patientDao.getById(patientId);
-        return visitDao.getVisitList(patient);
-    }
-
-    public List<Visit> getDoctorVisits(UUID doctorId) {
-        Doctor doctor = doctorDao.getById(doctorId);
-        return visitDao.getVisitList(doctor);
-    }
-
-    public boolean addVisit(Visit visit) {
-        return visitDao.addVisit(visit);
-    }
-
-    public boolean editVisit(UUID visitID, Visit editedVisit) {
-        return visitDao.editVisit(
-                visitID,
-                editedVisit.getPatientId(),
-                editedVisit.getDoctorId(),
-                editedVisit.getClinicId(),
-                editedVisit.getDate(),
-                editedVisit.isOnline(),
-                editedVisit.getReason(),
-                editedVisit.getStatus()
-        );
-    }
-
-    public boolean deleteVisit(UUID visitId) {
-        return visitDao.deleteVisit(visitDao.getVisit(visitId));
-    }
-
+    Visit getById(UUID id);
+    List<Visit> getPatientVisits(UUID patientId);
+    List<Visit> getDoctorVisits(UUID doctorId);
+    boolean addVisit(Visit visit);
+    boolean editVisit(UUID visitID, Visit editedVisit);
+    boolean deleteVisit(UUID visitId);
 }
