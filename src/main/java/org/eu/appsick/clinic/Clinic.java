3e3be2870345cf85d1240346d87b01d2fff285ac
Ochota Dariusz <ochota.dariusz@gmail.com>
2022-11-04 11:38:07 +0100

    Added clinic model

--- src/main/java/org/eu/appsick/clinic/Clinic.java
+++ src/main/java/org/eu/appsick/clinic/Clinic.java
@@ -1,4 +1,17 @@
 package org.eu.appsick.clinic;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
 public class Clinic {
+
+    private UUID clinicId;
+    private String clinicName;
+    private String longitude;
+    private String latitude;
+
 }
