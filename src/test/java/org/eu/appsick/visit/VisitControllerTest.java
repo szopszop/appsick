d0722a08dd5d00d308cdbaf02e5f034bac218a80
Sebastian Dudkowski <sebastian.dudkowski@gmail.com>
2022-11-15 00:36:56 +0100

    mock injected classes

--- src/test/java/org/eu/appsick/visit/VisitControllerTest.java
+++ src/test/java/org/eu/appsick/visit/VisitControllerTest.java
@@ -1,13 +1,42 @@
 package org.eu.appsick.visit;
 
+import org.eu.appsick.clinic.ClinicService;
+import org.eu.appsick.user.doctor.DoctorService;
+import org.eu.appsick.user.patient.PatientService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class VisitControllerTest {
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(VisitController.class)
+@ContextConfiguration(classes = {DoctorService.class, PatientService.class, ClinicService.class})
+public class VisitControllerTest {
+
+    private final VisitService visitService = Mockito.mock(VisitService.class);
+    private final DoctorService doctorService = Mockito.mock(DoctorService.class);
+    private final PatientService patientService = Mockito.mock(PatientService.class);
+    private final ClinicService clinicService = Mockito.mock(ClinicService.class);
+    private VisitController visitController;
+
+    @BeforeEach
+    void beforeEach(){
+        visitController = new VisitController(
+                visitService,
+                doctorService,
+                patientService,
+                clinicService
+        );
+    }
 
     @Test
-    void getVisit() {
+    void getBackJustAddedVisit() {
+
     }
 
     @Test
