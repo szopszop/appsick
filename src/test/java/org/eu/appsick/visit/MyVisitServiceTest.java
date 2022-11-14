8321bb64a9b1b7c4dc74597bc3d06d6cdc8e6595
Sebastian Dudkowski <sebastian.dudkowski@gmail.com>
2022-11-14 21:03:21 +0100

    create Test classes

---
+++ src/test/java/org/eu/appsick/visit/MyVisitServiceTest.java
@@ -0,0 +1,36 @@
+package org.eu.appsick.visit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyVisitServiceTest {
+
+    @Test
+    void getVisitById() {
+    }
+
+    @Test
+    void getPatientVisits() {
+    }
+
+    @Test
+    void getDoctorVisits() {
+    }
+
+    @Test
+    void getClinicVisits() {
+    }
+
+    @Test
+    void addVisit() {
+    }
+
+    @Test
+    void editVisit() {
+    }
+
+    @Test
+    void deleteVisit() {
+    }
+}
---
+++ src/test/java/org/eu/appsick/visit/VisitControllerTest.java
@@ -0,0 +1,36 @@
+package org.eu.appsick.visit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VisitControllerTest {
+
+    @Test
+    void getVisit() {
+    }
+
+    @Test
+    void getDoctorVisits() {
+    }
+
+    @Test
+    void getVisits() {
+    }
+
+    @Test
+    void getClinicVisits() {
+    }
+
+    @Test
+    void postVisit() {
+    }
+
+    @Test
+    void patchVisit() {
+    }
+
+    @Test
+    void deleteVisit() {
+    }
+}
