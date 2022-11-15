8599a95a12e14cf2f961af98aa406ef228eba807
Sebastian Dudkowski <sebastian.dudkowski@gmail.com>
2022-11-15 14:20:40 +0100

    add basic PUT, PATCH and DELETE methods tests

--- src/test/java/org/eu/appsick/visit/VisitControllerTest.java
+++ src/test/java/org/eu/appsick/visit/VisitControllerTest.java
@@ -7,10 +7,13 @@ import org.eu.appsick.user.doctor.DoctorService;
 import org.eu.appsick.user.patient.Patient;
 import org.eu.appsick.user.patient.PatientService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -58,7 +61,7 @@ public class VisitControllerTest {
         }
         Mockito.when(doctorService.getDoctorById(doctor.getDoctorId())).thenReturn(Optional.of(doctor));
         Mockito.when(visitService.getDoctorVisits(doctor)).thenReturn(visitList);
-        assertEquals((visitController.getDoctorVisits(doctor.getDoctorId())), visitList);
+        assertEquals(visitController.getDoctorVisits(doctor.getDoctorId()), visitList);
     }
 
     @Test
@@ -70,7 +73,7 @@ public class VisitControllerTest {
         }
         Mockito.when(patientService.getPatientById(patient.getPatientId())).thenReturn(Optional.of(patient));
         Mockito.when(visitService.getPatientVisits(patient)).thenReturn(visitList);
-        assertEquals((visitController.getPatientVisits(patient.getPatientId())), visitList);
+        assertEquals(visitController.getPatientVisits(patient.getPatientId()), visitList);
     }
 
     @Test
@@ -82,18 +85,45 @@ public class VisitControllerTest {
         }
         Mockito.when(clinicService.getClinicById(clinic.getClinicId())).thenReturn(Optional.of(clinic));
         Mockito.when(visitService.getClinicVisits(clinic)).thenReturn(visitList);
-        assertEquals((visitController.getClinicVisits(clinic.getClinicId())), visitList);
+        assertEquals(visitController.getClinicVisits(clinic.getClinicId()), visitList);
     }
 
     @Test
     void postVisit() {
+        Visit visit = new Visit();
+        ResponseEntity<Visit> expectedResponse = new ResponseEntity<>(visit, HttpStatus.OK);
+        Mockito.when(visitService.addVisit(visit)).thenReturn(true);
+        assertEquals(visitController.postVisit(visit), expectedResponse);
     }
 
     @Test
-    void patchVisit() {
+    @DisplayName("Patch Visit Returns HTTP Status OK when visit exists")
+    void patchVisitReturnsHttpStatusOkWhenVisitExists() {
+        Visit oldVisit = new Visit(1, null, null, null, null,
+                false, null, null);
+        Visit newVisit = new Visit(1, null, null, null, null,
+                true, null, null);
+        Mockito.when(visitService.editVisit(oldVisit.getVisitId(), newVisit)).thenReturn(true);
+        ResponseEntity<Visit> expectedResponse = new ResponseEntity<>(newVisit, HttpStatus.OK);
+        assertEquals(visitController.patchVisit(oldVisit.getVisitId(), newVisit), expectedResponse);
+    }
+    @Test
+    @DisplayName("Patch Visit Returns HTTP Status NOT FOUND when visit doesn't exist")
+    void patchVisitReturnsHttpStatusOkWhenVisitDoesNotExists() {
+        Visit oldVisit = new Visit(1, null, null, null, null,
+                false, null, null);
+        Visit newVisit = new Visit(1, null, null, null, null,
+                true, null, null);
+        Mockito.when(visitService.editVisit(oldVisit.getVisitId(), newVisit)).thenReturn(false);
+        ResponseEntity<Visit> expectedResponse = new ResponseEntity<>(newVisit, HttpStatus.NOT_FOUND);
+        assertEquals(visitController.patchVisit(oldVisit.getVisitId(), newVisit), expectedResponse);
     }
 
     @Test
     void deleteVisit() {
+        Visit visitToDelete = new Visit();
+        Mockito.when(visitService.deleteVisit(visitToDelete.getVisitId())).thenReturn(true);
+        ResponseEntity<Visit> expectedResponse = new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        assertEquals(visitController.deleteVisit(visitToDelete.getVisitId()), expectedResponse);
     }
 }
