9381e4c10c540b8dadf09b5636b426e48e8e429c
Sebastian Dudkowski <sebastian.dudkowski@gmail.com>
2022-11-15 13:51:19 +0100

    add basic GET methods tests

--- src/test/java/org/eu/appsick/visit/VisitControllerTest.java
+++ src/test/java/org/eu/appsick/visit/VisitControllerTest.java
@@ -1,7 +1,10 @@
 package org.eu.appsick.visit;
 
+import org.eu.appsick.clinic.Clinic;
 import org.eu.appsick.clinic.ClinicService;
+import org.eu.appsick.user.doctor.Doctor;
 import org.eu.appsick.user.doctor.DoctorService;
+import org.eu.appsick.user.patient.Patient;
 import org.eu.appsick.user.patient.PatientService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,6 +13,10 @@ import org.mockito.Mockito;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,7 +32,7 @@ public class VisitControllerTest {
     private VisitController visitController;
 
     @BeforeEach
-    void beforeEach(){
+    void init(){
         visitController = new VisitController(
                 visitService,
                 doctorService,
@@ -35,20 +42,47 @@ public class VisitControllerTest {
     }
 
     @Test
-    void getBackJustAddedVisit() {
-
+    void getVisit() {
+        Visit newVisit = new Visit();
+        long visitId = newVisit.getVisitId();
+        Mockito.when(visitService.getVisitById(visitId)).thenReturn(Optional.of(newVisit));
+        assertEquals(visitController.getVisit(visitId), Optional.of(newVisit));
     }
 
     @Test
     void getDoctorVisits() {
+        Doctor doctor = new Doctor();
+        List<Visit> visitList = new ArrayList<>();
+        for (int i=0; i<10; i++){
+            visitList.add(new Visit(0, null, doctor, null, null, false, null, null));
+        }
+        Mockito.when(doctorService.getDoctorById(doctor.getDoctorId())).thenReturn(Optional.of(doctor));
+        Mockito.when(visitService.getDoctorVisits(doctor)).thenReturn(visitList);
+        assertEquals((visitController.getDoctorVisits(doctor.getDoctorId())), visitList);
     }
 
     @Test
-    void getVisits() {
+    void getPatientVisits() {
+        Patient patient = new Patient();
+        List<Visit> visitList = new ArrayList<>();
+        for (int i=0; i<10; i++){
+            visitList.add(new Visit(0, patient, null, null, null, false, null, null));
+        }
+        Mockito.when(patientService.getPatientById(patient.getPatientId())).thenReturn(Optional.of(patient));
+        Mockito.when(visitService.getPatientVisits(patient)).thenReturn(visitList);
+        assertEquals((visitController.getPatientVisits(patient.getPatientId())), visitList);
     }
 
     @Test
     void getClinicVisits() {
+        Clinic clinic = new Clinic();
+        List<Visit> visitList = new ArrayList<>();
+        for (int i=0; i<10; i++){
+            visitList.add(new Visit(0, null, null, clinic, null, false, null, null));
+        }
+        Mockito.when(clinicService.getClinicById(clinic.getClinicId())).thenReturn(Optional.of(clinic));
+        Mockito.when(visitService.getClinicVisits(clinic)).thenReturn(visitList);
+        assertEquals((visitController.getClinicVisits(clinic.getClinicId())), visitList);
     }
 
     @Test
