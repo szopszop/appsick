d5540fc33c523f8b0307bc4e19fb6e4a17deb07d
Sebastian Dudkowski <sebastian.dudkowski@gmail.com>
2022-11-16 16:03:23 +0100

    add a dummy function to get doctors by clinic

--- src/main/java/org/eu/appsick/clinic/ClinicController.java
+++ src/main/java/org/eu/appsick/clinic/ClinicController.java
@@ -1,8 +1,12 @@
 package org.eu.appsick.clinic;
 
+import org.eu.appsick.user.doctor.Doctor;
+import org.eu.appsick.user.doctor.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -10,14 +14,24 @@ import java.util.Optional;
 public class ClinicController {
 
     private ClinicService clinicService;
+    private DoctorService doctorService;
 
     @Autowired
-    public ClinicController(ClinicService clinicService) {
+    public ClinicController(ClinicService clinicService, DoctorService doctorService) {
         this.clinicService = clinicService;
+        this.doctorService = doctorService;
     }
 
     @GetMapping("/{clinicId}")
     public Optional<Clinic> getClinicById(@PathVariable long clinicId) {
         return clinicService.getClinicById(clinicId);
+    }
+
+    // TODO: implement new methods, this is a dummy function
+    @GetMapping("/{clinicId}/doctor")
+    public List<Doctor> getDoctorsByClinic(@PathVariable long clinicId) {
+        List<Doctor> doctors = new ArrayList<>();
+        doctors.add(doctorService.getDoctorById(1).orElse(new Doctor()));
+        return doctors;
     }
 }
