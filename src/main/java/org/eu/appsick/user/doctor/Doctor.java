63c54c0b84f1c53865eb5a395d6b2b9bb72afee5
Ochota Dariusz <ochota.dariusz@gmail.com>
2022-11-28 14:42:21 +0100

    Doctor specialities not anymore in uppercase

--- src/main/java/org/eu/appsick/user/doctor/Doctor.java
+++ src/main/java/org/eu/appsick/user/doctor/Doctor.java
@@ -1,6 +1,7 @@
 package org.eu.appsick.user.doctor;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -74,6 +75,7 @@ public class Doctor {
             this.speciality = speciality;
         }
 
+        @JsonValue
         @Override
         public String toString() {
             return speciality;
