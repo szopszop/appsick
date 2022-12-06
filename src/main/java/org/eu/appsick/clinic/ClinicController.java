e52cbdff62011de945dc9a8642e043236d0d5cc5
Ochota Dariusz <ochota.dariusz@gmail.com>
2022-12-06 14:36:08 +0100

    Fix response cookie

--- src/main/java/org/eu/appsick/clinic/ClinicController.java
+++ src/main/java/org/eu/appsick/clinic/ClinicController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/clinic")
-@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*", allowCredentials = "true")
+@CrossOrigin(origins = "https://appsick.eu.org", allowedHeaders = "*", allowCredentials = "true")
 public class ClinicController {
 
     private final ClinicService clinicService;
