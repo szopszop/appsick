cb638242b7df2b499630db9696c66f8e5158b501
Sebastian Dudkowski <sebastian.dudkowski@gmail.com>
2022-12-10 18:44:56 +0100

    declare getDoctorsByClinic() function

--- src/main/java/org/eu/appsick/user/doctor/DoctorService.java
+++ src/main/java/org/eu/appsick/user/doctor/DoctorService.java
@@ -1,8 +1,10 @@
 package org.eu.appsick.user.doctor;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DoctorService {
 
     Optional<Doctor> getDoctorById(Long doctorId);
+    List<Doctor> getDoctorsByClinic(Long clinicId);
 }
