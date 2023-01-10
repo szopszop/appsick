27d4845ed59bf73ecdcfeeae3dcfae9e133c7eb1
Sebastian Dudkowski <sebastian.dudkowski@gmail.com>
2023-01-10 19:27:44 +0100

    write ClinicService tests

--- src/test/java/org/eu/appsick/clinic/ClinicServiceTest.java
+++ src/test/java/org/eu/appsick/clinic/ClinicServiceTest.java
@@ -1,11 +1,20 @@
 package org.eu.appsick.clinic;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(ClinicService.class)
@@ -15,11 +24,43 @@ public class ClinicServiceTest {
     @Mock
     private ClinicRepository clinicRepository;
     private ClinicService clinicService;
+    private final Random random = new Random();
 
     @BeforeEach
     void init(){
         clinicService = new MyClinicService(clinicRepository);
     }
 
+    @Test
+    void getClinicById(){
+        Clinic clinic = new Clinic(
+                random.nextLong(),
+                "Test Clinic",
+                "0",
+                "0",
+                null,
+                null);
+        Mockito.when(clinicRepository.findByClinicId(clinic.getClinicId()))
+                .thenReturn(Optional.of(clinic));
+        assertEquals(clinicService.getClinicById(clinic.getClinicId()), Optional.of(clinic));
+    }
+
+    @Test
+    void getAllClinics(){
+        List<Clinic> clinics = new ArrayList<>();
+        for (int i=0; i<10; i++){
+            clinics.add(
+                    new Clinic(
+                            random.nextLong(),
+                            "Test Clinic " + i,
+                            "0",
+                            "0",
+                            null,
+                            null)
+            );
+        }
+        Mockito.when(clinicRepository.findAll()).thenReturn(clinics);
+        assertEquals(clinicService.getAllClinics(), clinics);
+    }
 
 }
