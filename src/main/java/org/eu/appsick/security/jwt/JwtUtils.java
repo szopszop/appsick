142c1ff77ec55fbc22d6e98c0d4125f423273a3a
PrWojciech <98315478+PrWojciech@users.noreply.github.com>
2023-03-21 10:53:57 +0100

    Update src/main/java/org/eu/appsick/security/jwt/JwtUtils.java
    Co-authored-by: Dariusz Ochota <ochota.dariusz@gmail.com>

--- src/main/java/org/eu/appsick/security/jwt/JwtUtils.java
+++ src/main/java/org/eu/appsick/security/jwt/JwtUtils.java
@@ -45,7 +45,6 @@ public class JwtUtils {
                              .path("/api")
                              .maxAge(7 * 24 * 60 * 60)
                              .httpOnly(true)
-                             .build();
     }
 
     public ResponseCookie getCleanJwtCookie() {
